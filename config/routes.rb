d7845a5ba5b58ce4e35e79e91593fbc3ffd9266d
kazuki-oda <oda.kazuki19970317@gmail.com>
2020-04-15 15:10:02 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root  "groups#index"
+  root "messages#index"
   resources :users, only: [:new, :edit, :update, :destroy]
   resources :groups, only: [:index] do
     resources :messages, only: [:index, :new, :create] do 
